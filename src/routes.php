400e4b1a0ae87374f3839d80b4f6c9434f3efbcc
Lucas Pimenta <lucas.pimenta94@outlook.com>
2019-07-17 23:25:17 -0300

    initial commit

---
+++ src/routes.php
@@ -0,0 +1,34 @@
+<?php
+
+use Slim\App;
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+return function (App $app) {
+    $container = $app->getContainer();
+
+    $app->get('/', function (Request $request, Response $response, array $args) use ($container) {
+        // Sample log message
+        $container->get('logger')->info("Slim-Skeleton '/' route");
+
+        // Render index view
+        return $container->get('renderer')->render($response, 'index.phtml', $args);
+    });
+
+    $app->get('/name', function (Request $request, Response $response, array $args) use ($container) {
+        // Sample log message
+        $container->get('logger')->info("Slim-Skeleton '/name' route");
+
+        // Render index view
+        return $container->get('renderer')->render($response, 'index.phtml', $args);
+    });
+
+    $app->get('/abacate', function (Request $request, Response $response, array $args) use ($container) {
+        // Sample log message
+        $container->get('logger')->info("Slim-Skeleton '/abacate' route");
+        
+        $response = array('bla' => 'blabla');
+
+        echo json_encode($response);
+    });
+};
