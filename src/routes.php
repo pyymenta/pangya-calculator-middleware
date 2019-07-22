30eceda03a45c88818be37e81813a4473256e2b3
Lucas Pimenta <lucas.pimenta94@outlook.com>
2019-07-21 23:28:24 -0300

    add pin/backspin route

--- src/routes.php
+++ src/routes.php
@@ -1,34 +1,37 @@
 <?php
+include('PangyaCalculateMiddleware/PinGenerator.php');
 
 use Slim\App;
 use Slim\Http\Request;
 use Slim\Http\Response;
 
+
+
+use PangyaCalculateMiddleware\PinGenerator;
+
 return function (App $app) {
     $container = $app->getContainer();
 
-    $app->get('/', function (Request $request, Response $response, array $args) use ($container) {
-        // Sample log message
-        $container->get('logger')->info("Slim-Skeleton '/' route");
+    $app->group('/pins',
+        function () {
+            $this->get('/backspin',
+                function (Request $request, Response $response, array $args) use ($container) {
+                    // Sample log message
 
+                    $generator = new PinGenerator();
+                    $response = $generator->getGeneratorSimpleValue('bs_1W.xls', 'backspin');
+
+                    return json_encode($response);
+                }
+            );
+        }
+    );
+
+    $app->get('/[{name}]',
+    function (Request $request, Response $response, array $args) {
+        // Sample log message
+        $this->logger->info("Slim-Skeleton '/' route");
         // Render index view
-        return $container->get('renderer')->render($response, 'index.phtml', $args);
-    });
-
-    $app->get('/name', function (Request $request, Response $response, array $args) use ($container) {
-        // Sample log message
-        $container->get('logger')->info("Slim-Skeleton '/name' route");
-
-        // Render index view
-        return $container->get('renderer')->render($response, 'index.phtml', $args);
-    });
-
-    $app->get('/abacate', function (Request $request, Response $response, array $args) use ($container) {
-        // Sample log message
-        $container->get('logger')->info("Slim-Skeleton '/abacate' route");
-
-        $response = array('bla' => 'blabla');
-
-        return json_encode($response);
+        return $this->renderer->render($response, 'index.phtml', $args);
     });
 };
